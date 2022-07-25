5e6ab432f59f40ba98a30f18cfa5130d323b78e9
Zyad Ahmed <68651350+ZyadSaber@users.noreply.github.com>
2022-07-25 18:00:11 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,24 +5,13 @@ let index_to_edit
 let save_edit = false
 const exsys_get_api = "http://207.180.237.36:9090/ords/exsys_api/ex_react_test_emp/exsys_react_test_emp_data?authorization=3995005&staff_id=&poffset=0&staff_short_code&staff_name="
 const exsys_post_api = "http://207.180.237.36:9090/ords/exsys_api/ex_react_test_emp/exsys_react_test_emp_dml"
-let new_object = {
-    "total": 5,
-    "age": 25,
-    "date_start_service": "01-07-2022",
-    "email": "ABC@GMAIL.COM",
-    "genger": "M",
-    "staff_id": 1,
-    "staff_name": "ALI ZAKI",
-    "address": " CAIRO EGYPT",
-    "staff_short_code": "AZ",
-    "record_status": "q"
-}
+
 let new_array = []
-let end_obj
+let ap
 // Global Functions
 function log (message) {console.log(message)};
 function state (i) {
-    if (new_array[i].status === "O") {
+    if (new_array[i].genger === "M") {
         return "checked"
 }else{
     return ""
@@ -30,7 +19,7 @@ function state (i) {
 };
 function ch_state () {
     if (document.getElementById("checkbox").checked) {
-        return "O"
+        return "M"
 }
 };
 function clr () {
@@ -39,16 +28,34 @@ function clr () {
 function rows () {
     table.innerHTML += row
 }
-function save (ex) {
+function save (staff_name, age, date_start_service, email,  address, state, record_status, staff_id) {
 
     fetch(exsys_post_api,{
         method: "POST",
         headers: {
             'content-type' : 'application/json'
         },
-        body: JSON.stringify(ex)
+        body: JSON.stringify(
+            {
+                "authorization": "201792", 
+	            "data": [
+        {
+            "age": age,
+            "date_start_service": date_start_service,
+            "email": email,
+            "genger": state,
+            "staff_id": staff_id,
+            "staff_name": staff_name,
+            "staff_short_code": "AZ",
+            "address": address,
+            "record_status": record_status
+		}]
+            }
+        )
     }
         )
+        .then(res => res.json())
+        .then(generate())
     
 };
 
@@ -59,7 +66,7 @@ function send_date (date) {
     return `${d}-${m}-${y}`
 };
 function show_date (date) {
-    let b = new_array[date].date_of_birth
+    let b = new_array[date].date_start_service
     if (b !== undefined) {
         let y = b.slice(6,10)
         let m =b.slice(3,5)
@@ -73,15 +80,12 @@ function show_date (date) {
 
 // Load all the available data when the page is loaded
 
-
-
-
 function generate () {
     fetch(exsys_get_api)
 .then((s) => s.json())
 .then((arr) => {
     new_array = arr.data
-    end_obj = {"data":new_array}
+    // end_obj = {"data":new_array}
     log(new_array)
     for (let i = 0; i < new_array.length; i++){
             row = `<tr> 
@@ -115,7 +119,7 @@ function add_data_fun (data) {
             <td class="age">${data[i].age}</td>
             <td class="email">${data[i].email}</td>
             <td class="address">${data[i].address}</td>
-            <td class="date"><input type="date" disabled"/></td>
+            <td class="date"><input type="date" disabled value="${show_date(i)}"/></td>
             <td class="checkbox"><input type="checkbox" ${state(i)} disabled/></td>
             <td> <div class="d_e"><button id="delete" disabled onclick="delete_this_data(${[i]})">❌</button><button class="edit" disabled onclick="edit_this_data(${[i]}, my_data)">🖋</button></div></td>
             </tr>`;
@@ -127,7 +131,7 @@ function add_data_fun (data) {
             <td><input type="text" id="age"></td>
             <td><input type="email" id="email"></td>
             <td><input type="text" id="address"></td>
-            <td class="date"><input type="Date" id="date_of_birth" required pattern="\d{4}-\d{2}-\d{2}"></td>
+            <td class="date"><input type="date" disabled" id="date_of_birth"s/></td>
             <td><input type="checkbox" id="checkbox"></td>
             <td> <div class="d_e"><button disabled id="delete" onclick="delete_this_data()">❌</button><button disabled class="edit" onclick="edit_this_data()">🖋</button></div></td>
             </tr>`;
@@ -141,39 +145,35 @@ function add_data_fun (data) {
 // edit button to the data
 
 function edit_this_data (i, data) {
+    ab = data[i].staff_id
     save_edit = true
     btn_edit = true
     table.innerHTML = ""
     document.getElementById("add_data").setAttributeNode(document.createAttribute("disabled"))
     document.getElementById("save_data").disabled="";
      for (let e = 0; e < data.length; e++){
-        if (data[i].record_status === "D"){
-            continue
-        }else if (e != i){
+     if (e != i){
             row = `<tr> 
             <td>${data[e].staff_name}</td>
             <td>${data[e].age}</td>
             <td>${data[e].email}</td>
             <td>${data[e].address}</td>
-            <td><input type="date" disabled /></td>
+            <td><input type="date" disabled value="${show_date(i)}"/></td>
             <td><input type="checkbox" disabled ${state(e)}/></td>
             <td><div class="d_e"><button id="delete" disabled>❌</button><button class="edit" disabled>🖋</button></dev></td>
             </tr>`;
         } else {
-            row = `<tr> 
+            row = `<tr>
             <td><input type="text" required id="name" value="${data[e].staff_name}"></td>
             <td><input type="text" id="age" value="${data[e].age}"></td>
             <td><input type="email" id="email" value="${data[e].email}"></td>
             <td><input type="text" id="address" value="${data[e].address}"></td>
-            <td><input type="Date" id="date_of_birth" required pattern="\d{4}-\d{2}-\d{2}" ></td>
+            <td><input type="Date" id="date_of_birth" required pattern="\d{4}-\d{2}-\d{2}" value="${show_date(i)}"></td>
             <td><input type="checkbox" id="checkbox" ${state(e)}></td>
             <td><div class="d_e"><button id="delete" disabled" disabled>❌</button><button class="edit"disabled>🖋</button></dev></td>
             </tr>`;
             index_to_edit = e
-            log(index_to_edit)
         }
-
-
             rows()
             }
             
@@ -185,67 +185,46 @@ function edit_this_data (i, data) {
 
 function save_data_fun () {
     if (save_edit === false) {
-        let staff_name = document.getElementById("name").value;
-        let age = document.getElementById("age").value;
-        let email = document.getElementById("email").value;
-        let address = document.getElementById("address").value;
-        let date_of_birth  = document.getElementById("date_of_birth").value;
-        if (staff_name === "" || age === "" || email  === "" || address  === "" ) {
-            document.getElementById("err_msg").hidden="";
-        } else {
-            document.getElementById("err_msg").setAttributeNode(document.createAttribute("hidden"));
-            document.getElementById("save_data").setAttributeNode(document.createAttribute("disabled"));
-            document.getElementById("add_data").disabled="";
-            let new_array_data ={
-                "address": address,
-                "age": age,
-                "date_start_service": send_date(date_of_birth),
-                "email": email,
-                "genger": "M",
-                "record_status" :"n",
-                "staff_id": 1,
-                "staff_name": staff_name,
-                "staff_name": staff_name,
-                "status" : ch_state(),
+        document.getElementById("err_msg").setAttributeNode(document.createAttribute("hidden"));
+        document.getElementById("save_data").setAttributeNode(document.createAttribute("disabled"));
+        document.getElementById("add_data").disabled="";
+        let staff_name, age, email, address, date_of_service, state
+        staff_name = document.getElementById("name").value;
+        age = document.getElementById("age").value;
+        email = document.getElementById("email").value;
+        address = document.getElementById("address").value;
+        date_start_service  = send_date(document.getElementById("date_of_birth").value)
+        if (document.getElementById("checkbox").checked) {
+                state = "M"
+            }else {
+               state = "";
             };
-            new_array.push(new_array_data);
+           
             clr()
-            save(end_obj);
-            generate();
-        }
+            save(staff_name, age, date_start_service, email,  address, state, "n");
+        // generate()
     }
 };
 function save_edited_data() {
     if (save_edit === true) {
-        let name = document.getElementById("name").value;
-        let age = document.getElementById("age").value;
-        let email = document.getElementById("email").value;
-        let address = document.getElementById("address").value;
-        let date_of_birth  = document.getElementById("date_of_birth").value;    
-        if (name === "" || age === "" || email  === "" || address  === "" ) {
-            document.getElementById("err_msg").hidden="";
-        } else {
+        let staff_name, age, email, address, date_start_service, state
+        staff_name = document.getElementById("name").value;
+        age = document.getElementById("age").value;
+        email = document.getElementById("email").value;
+        address = document.getElementById("address").value;
+        date_start_service = send_date(document.getElementById("date_of_birth").value) ;    
         document.getElementById("err_msg").setAttributeNode(document.createAttribute("hidden"));
         document.getElementById("save_data").setAttributeNode(document.createAttribute("disabled"))
         document.getElementById("add_data").disabled="";
-        console.log("edit")
-        new_array[index_to_edit].address = address;
-        new_array[index_to_edit].age = age;
-        new_array[index_to_edit].date_of_birth = send_date(date_of_birth);
-        new_array[index_to_edit].email = email;
-        new_array[index_to_edit].genger= "M",
-        new_array[index_to_edit].record_status = "u"
-        new_array[index_to_edit].staff_id = new_array[index_to_edit].staff_id
-        new_array[index_to_edit].staff_name = name;
         if (document.getElementById("checkbox").checked) {
-                new_array[index_to_edit].state = "checked";
+                state = "M"
             }else {
-                new_array[index_to_edit].state = "";
+               state = "";
             };
         clr()
-        save(end_obj);
-        generate();
-        }
+        ab = new_array[index_to_edit].staff_id
+        save(staff_name, age, date_start_service, email,  address, state, "u", ab);
+        // generate()
     }
         
 };
@@ -255,13 +234,9 @@ function save_edited_data() {
 // delete specific data
 
 function delete_this_data (i) {
-    new_array[i].staff_name = ""
-    new_array[i].age = ""
-    new_array[i].email = ""
-    new_array[i].address = ""
-    new_array[i].date_of_birth = ""
-    new_array[i].record_status = "d"
-     clr();
-    save(end_obj);
-    generate();
+    let staff_name, age, email, address, date_start_service, state
+    ab = new_array[i].staff_id 
+    clr();
+    save(staff_name, age, date_start_service, email,  address, state, "d", ab);
+    
 };
